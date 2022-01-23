cb708f87a4a6512f695626e2dee9f06b25c9ae05
Fengshan Yu <95190262+Fengshan-yu@users.noreply.github.com>
2022-01-23 07:18:14 -0500

    Add files via upload

---
+++ General_Zhang.py
@@ -0,0 +1,25 @@
+from bs4 import BeautifulSoup as bs
+import requests
+
+url = "https://en.wikipedia.org/wiki/Zhang_Zizhong"
+data = requests.get(url).text
+soup = bs(data, 'html.parser')
+
+tag = soup.find("body")
+info_boxes = tag.find(class_="infobox vcard")
+info_rows = info_boxes.find_all("tr")
+
+General_info = {}
+
+for index, row in enumerate(info_rows):
+    if index <= 1:
+        continue
+    elif index == 2:
+        General_info["姓名"] = info_boxes.find("div", attrs={"class": "nickname"}).get_text()
+
+    else:
+        dict_key = row.find("th").get_text().replace("\xa0", " ")
+        dict_value = row.find("td").get_text(" ", strip=True).replace("\xa0", " ").replace("†", "（殉国）")
+        General_info[dict_key] = dict_value
+
+print(General_info)
